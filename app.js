4ef46191f2c078818e2614624f3f18c3f4603fc1
HanVincent <vincent.han.tw@gmail.com>
2018-01-10 01:34:13 +0800

    Add features and beautify UI
    1. Auto detect selection and request it
    2. Add loding mask for UX
    3. Beautify UI
    -
    TODO:
    1. Multiple requests in a short time

--- app.js
+++ app.js
@@ -1,222 +1,70 @@
 'use strict';
 
+(function() {
+  // API_URL = 'http://nlp-ryze.cs.nthu.edu.tw:1214/translate/'
+  var API_URL = 'https://fathomless-wave-32876.herokuapp.com/messages';
+  var HEADERS = {
+    'Content-Type': 'application/json; charset=UTF-8',
+    'Access-Control-Allow-Origin': '*',
+  };
+  var loadingMask = null;
 
+  // The initialize function is run each time the page is loaded.
+  Office.initialize = function(reason) {
 
-(function () {
-    // API_URL = 'http://nlp-ryze.cs.nthu.edu.tw:1214/translate/'
-    var API_URL = 'https://fathomless-wave-32876.herokuapp.com/messages';
-    var HEADERS = {'Content-Type': 'application/json; charset=UTF-8', 'Access-Control-Allow-Origin': '*'}
-   
-    // The initialize function is run each time the page is loaded.
-    Office.initialize = function (reason) {
-        $(document).ready(function () {
+    $(document).ready(function() {
+      // Use this to check whether the API is supported in the Word client.
+      if (Office.context.requirements.isSetSupported('WordApi', 1.1)) {
+        Office.context.document.addHandlerAsync('documentSelectionChanged',
+            detectChange, // handler
+            function(result) {} // callback
+        );
+        loadingMask = $('#loading-mask');
+        loadingMask.hide();
+        $('#supportedVersion').html('This code is using Word 2016 or greater.');
+      }
+      else {
+        $('#supportedVersion').html('This code requires Word 2016 or greater.');
+      }
+    });
+  };
 
-            // Use this to check whether the API is supported in the Word client.
-            if (Office.context.requirements.isSetSupported('WordApi', 1.1)) {
-                // Do something that is only available via the new APIs
-                $('#emerson').click(insertEmersonQuoteAtSelection);
-                $('#checkhov').click(insertChekhovQuoteAtTheBeginning);
-                $('#proverb').click(insertChineseProverbAtTheEnd);
-                $('#run').click(goToMainPage);
-               
-                $('#supportedVersion').html('This code is using Word 2016 or greater.');
-            }
-            else {
-                // Just letting you know that this code will not work with your version of Word.
-                $('#supportedVersion').html('This code requires Word 2016 or greater.');
-            }
+  function detectChange(eventArgs) {
+    eventArgs.document.getSelectedDataAsync(
+        Office.CoercionType.Text,
+        function(asyncResult) {
+          if (asyncResult.status == Office.AsyncResultStatus.Failed) {
+            console.error('Action failed. Error: ' +
+                asyncResult.error.message);
+          } else if (asyncResult.value.trim()) {
+            request(asyncResult.value); // get content and ajax
+          }
         });
-    };
+  }
 
-    function insertEmersonQuoteAtSelection() {
-        Word.run(function (context) {
+  function request(query) {
+    $('#select-sec').html(query);
+    $.ajax({
+      type: 'POST',
+      url: API_URL,
+      data: JSON.stringify({text: query}),
+      dataType: 'json',
+      headers: HEADERS,
+      beforeSend: function() {loadingMask.show();},
+      complete: function() {loadingMask.hide();},
+      success: showCorrect,
+      error: function(XMLHttpRequest) {
+        console.error(XMLHttpRequest);
+        $('#correct-sec').html('Something wrong, please reopen it.');
+      },
+    });
+  }
 
-            // Create a proxy object for the document.
-            var thisDocument = context.document;
+  function showCorrect(data) {
+    var content = data.word_diff.replace(/\[-(.*?)-\]/g,
+        '<span class="deletion">$1</span>').
+        replace(/\{\+(.+?)\+\}/g, '<span class="correction">$1</span>');
 
-            // Queue a command to get the current selection.
-            // Create a proxy range object for the selection.
-            var range = thisDocument.getSelection();
-
-            // Queue a command to replace the selected text.
-            range.insertText('"Hitch your wagon to a star."\n', Word.InsertLocation.replace);
-
-            // Synchronize the document state by executing the queued commands,
-            // and return a promise to indicate task completion.
-            return context.sync().then(function () {
-                console.log('Added a quote from Ralph Waldo Emerson.');
-            });
-        })
-        .catch(function (error) {
-            console.log('Error: ' + JSON.stringify(error));
-            if (error instanceof OfficeExtension.Error) {
-                console.log('Debug info: ' + JSON.stringify(error.debugInfo));
-            }
-        });
-    }
-    function helloH(){
-        console.log("hello")
-        $.ajax({
-            type: "POST",
-            url: API_URL,
-            //data: {'text': query},
-            data: JSON.stringify({text: "discuss about the issue."}),
-            headers: HEADERS,
-            // contentType: 'application/json',
-            //contentType: HEADERS,
-            dataType: 'json',
-            success: function (data) {
-                console.log("success");
-                $('#show').html("success");
-             
-                // document.getElementById("show").textContent = "success: " + data; 
-               
-                // document.getElementById("show").textContent = data.result;
-            }, 
-            error: function(XMLHttpRequest, textStatus, errorThrown) { 
-                console.log("Status: " + textStatus); 
-                console.log("Error: " + errorThrown);
-                $('#show').html("error: "+ +JSON.stringify( errorThrown )+"<br>Request:"+ JSON.stringify(XMLHttpRequest));
-              //   document.getElementById("show").textContent = "error: " +  errorThrown.val(); 
-            } 
-          })
-
-    }
-    function goToMainPage(){
-       
-        Word.run(function (context) {
-            var thisDocument = context.document
-            var range = thisDocument.getSelection()
-            context.load(range, 'text');
-
-            return context.sync().then(function () {
-               // document.getElementById("show").textContent = range.text; 
-               gec_it_post(range.text)
-            });  
-
-            
-            // gec_it_post("You are an girl.");
-
-            // Create a proxy object for the document.
-           
-            
-           //  document.getElementById("show").textContent = thisDocument.textContent;
-
-
-            // var paragraphs = context.document.getSelection().paragraphs;
-            // paragraphs.load();
-            // return context.sync().then(function () {
-                   
-            //     document.getElementById("show").textContent = paragraphs;
-            //     // paragraphs.items[0].insertText(' New sentence in the paragraph.',
-            //     //                             Word.InsertLocation.end);
-            // }).then(context.sync);
-
-
-
-            // Queue a command to get the current selection.
-            // Create a proxy range object for the selection.
-          
-
-           // get_it_post(thisDocument);
-
-            // // Queue a command to replace the selected text.
-            // range.insertText('"Hitch your wagon to a star."\n', Word.InsertLocation.replace);
-
-            // Synchronize the document state by executing the queued commands,
-            // and return a promise to indicate task completion.
-            return context.sync().then(function () {
-                console.log('Added a quote from Ralph Waldo Emerson.');
-            });
-        })
-        .catch(function (error) {
-            console.log('Error: ' + JSON.stringify(error));
-            if (error instanceof OfficeExtension.Error) {
-                console.log('Debug info: ' + JSON.stringify(error.debugInfo));
-            }
-        });
-
-    }
-
-    function gec_it_post(query){
-        
-        // document.getElementById("ask").textContent = "Question:" + query + "<br>";
-        $('#ask').html( query + "<br><br>");
-        $.ajax({
-            type: "POST",
-            url: API_URL,
-            //data: {'text': query},
-            data: JSON.stringify({text: query}),
-            headers: HEADERS,
-            // contentType: 'application/json',
-            //contentType: HEADERS,
-            dataType: 'json',
-            success: function (data) {
-                console.log("success");
-                $('#show').html(data.result);
-             
-               //  document.getElementById("show").textContent = "success: " + data; 
-               
-                // document.getElementById("show").textContent = data.result;
-            }, 
-            error: function(XMLHttpRequest, textStatus, errorThrown) { 
-                console.log("Status: " + textStatus); 
-                console.log("Error: " + errorThrown);
-                $('#show').html("error");
-              //   document.getElementById("show").textContent = "error: " +  errorThrown.val(); 
-            } 
-          })
-	}
-
-    function insertChekhovQuoteAtTheBeginning() {
-        Word.run(function (context) {
-
-            // Create a proxy object for the document body.
-            var body = context.document.body;
-
-            // Queue a command to insert text at the start of the document body.
-            body.insertText('"Knowledge is of no value unless you put it into practice."\n', Word.InsertLocation.start);
-
-            // Synchronize the document state by executing the queued commands,
-            // and return a promise to indicate task completion.
-            return context.sync().then(function () {
-                console.log('Added a quote from Anton Chekhov.');
-            });
-        })
-        .catch(function (error) {
-            console.log('Error: ' + JSON.stringify(error));
-            if (error instanceof OfficeExtension.Error) {
-                console.log('Debug info: ' + JSON.stringify(error.debugInfo));
-            }
-        });
-    }
-
-    function insertChineseProverbAtTheEnd() {
-        Word.run(function (context) {
-
-            // Create a proxy object for the document body.
-            var body = context.document.body;
-
-            // Queue a command to insert text at the end of the document body.
-            body.insertText('"To know the road ahead, ask those coming back."\n', Word.InsertLocation.end);
-
-            // Synchronize the document state by executing the queued commands,
-            // and return a promise to indicate task completion.
-            return context.sync().then(function () {
-                console.log('Added a quote from a Chinese proverb.');
-            });
-        })
-        .catch(function (error) {
-            console.log('Error: ' + JSON.stringify(error));
-            if (error instanceof OfficeExtension.Error) {
-                console.log('Debug info: ' + JSON.stringify(error.debugInfo));
-            }
-        });
-    }
+    $('#correct-sec').html(content);
+  }
 })();
-
-
-
-// $( document ).ready(function() {
-//     $('#hello').click(helloH);
-// });
